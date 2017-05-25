1b767b8b5b0de3d54b7cad58d459832dd90f890d
Tom Butler <t@shedcollective.org>
2017-05-25 23:58:36 +0100

    styles etc

--- user/pages/02.profile/profile.md
+++ user/pages/02.profile/profile.md
@@ -1,7 +1,7 @@
 ---
 title: Profile
-heading: Profile
-introduction: 'We left the busy South East of England and took on the challenge of this 3 acre smallholding in the Wild West of Wales in January 2016. We had been looking for a while for a place to give us space to try out our ideas and the land to give us the opportunity to pool our skills, knowledge and backgrounds and to try to create a life for our family that embraced the essence of nature, farming and self-sufficiency.'
+heading: 'My name is Tom Butler. I am an Interaction designer/developer based in London and currently leading design at Shed Collective. '
+introduction: 'My name is Tom Butler. I am an Interaction designer/developer based in London and currently leading design at Shed Collective. '
 body_classes: inverted
 ---
 
--- user/themes/tom-butler/js/app.js
+++ user/themes/tom-butler/js/app.js
@@ -1,23 +1,23 @@
-$(document).ready(function() {
-  $(".animsition").animsition({
-    inClass: 'fade-in',
-    outClass: 'fade-out',
-    inDuration: 800,
-    outDuration: 800,
-    linkElement: '.animsition-link',
-    loading: true,
-    loadingParentElement: 'body',
-    loadingClass: 'animsition-loading',
-    loadingInner: '<img src="/user/themes/tom-butler/images/logo.svg">', // e.g '<img src="loading.svg" />'
-    timeout: true,
-    timeoutCountdown: 5000,
-    onLoadEvent: true,
-    browser: [ 'animation-duration', '-webkit-animation-duration'],
-    // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
-    // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
-    overlay : false,
-    overlayClass : 'animsition-overlay-slide',
-    overlayParentElement : 'body',
-    transition: function(url){ window.location.href = url; }
-  });
-});
+// $(document).ready(function() {
+//   $(".animsition").animsition({
+//     inClass: 'fade-in',
+//     outClass: 'fade-out',
+//     inDuration: 800,
+//     outDuration: 800,
+//     linkElement: '.animsition-link',
+//     loading: true,
+//     loadingParentElement: 'body',
+//     loadingClass: 'animsition-loading',
+//     loadingInner: '<img src="/user/themes/tom-butler/images/logo.svg">', // e.g '<img src="loading.svg" />'
+//     timeout: true,
+//     timeoutCountdown: 5000,
+//     onLoadEvent: true,
+//     browser: [ 'animation-duration', '-webkit-animation-duration'],
+//     // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+//     // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+//     overlay : false,
+//     overlayClass : 'animsition-overlay-slide',
+//     overlayParentElement : 'body',
+//     transition: function(url){ window.location.href = url; }
+//   });
+// });
